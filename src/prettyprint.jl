f5cd9bacb21fe5c24857a501bd8bd108c93f7dea
Michael Creel <michael.creel@uab.es>
2017-05-30 16:06:50 +0200

    minor update

--- src/prettyprint.jl
+++ src/prettyprint.jl
@@ -1,18 +1,20 @@
 # formatted print of array, with column names
-function prettyprint(a, cnames, rnames="",digits=8, decimals=4)
+function prettyprint(a, cnames="", rnames="",digits=8, decimals=4)
     # TBD: try to use this to allow using specified digits and decimals
     #fmt = @sprintf("%d",digits)"."@sprintf("%d",decimals)"%f"
     #@eval dofmt(x) = @sprintf($fmt, x)
     
     # print column names
-    for i = 1:size(a,2)
-        pad = digits
-        if rnames != "" && i==1
-            pad = 2*digits
-        end    
-        @printf("%s", lpad(cnames[i],pad," "))
-    end
-    @printf("\n")
+    if cnames != ""
+        for i = 1:size(a,2)
+            pad = digits
+            if rnames != "" && i==1
+                pad = 2*digits
+            end    
+            @printf("%s", lpad(cnames[i],pad," "))
+        end
+        @printf("\n")
+    end    
     # print the rows
     for i = 1:size(a,1)
         if rnames != ""
