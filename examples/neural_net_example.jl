c263b382c5e3120a7a5e6a2a796710ec2065706f
Michael Creel <michael.creel@uab.es>
2017-09-22 14:02:02 +0200

    need to update MXnet usage, comment out for now

--- examples/neural_net_example.jl
+++ examples/neural_net_example.jl
@@ -14,27 +14,27 @@ function make_simdata(reps=100000)
         z = [ones(n,1) x]
         # draw the parameters from prior
         b = randn(5,1)
-        sig = exp(randn(1))
+        sig = exp.(randn(1))
         # generate dependent variable
         y = z*b + sig.*randn(n,1)
         # linear model
         bhat1 = z\y
         uhat = y-z*bhat1
-        sighat1 = sqrt(uhat'*uhat/(n-size(z,2)))
+        sighat1 = sqrt.(uhat'*uhat/(n-size(z,2)))
         # quadratic model
         z = [ones(n,1) x 0.1*x.^2.0]
         bhat2 = z\y
         uhat = y-z*bhat2
-        sighat2 = sqrt(uhat'*uhat/(n-size(z,2)))
+        sighat2 = sqrt.(uhat'*uhat/(n-size(z,2)))
         # cubic model
         z = [ones(n,1) x 0.1*x.^2.0 0.01*x.^3.0]
         bhat3 = z\y
         uhat = y-z*bhat3
-        sighat3 = sqrt(uhat'*uhat/(n-size(z,2)))
+        sighat3 = sqrt.(uhat'*uhat/(n-size(z,2)))
         # pure noise
         z = randn(1,5)
         # assemble: 
-        simdata[rep,:] = [b' log(sig) bhat1' log(sighat1) bhat2' log(sighat2) bhat3' log(sighat3) z]
+        simdata[rep,:] = [b' log.(sig) bhat1' log.(sighat1) bhat2' log.(sighat2) bhat3' log.(sighat3) z]
     end
     return simdata
 end
