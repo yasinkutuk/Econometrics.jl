da89f5d09681b3ffee588de5945e3f7485ef77d4
Michael Creel <michael.creel@uab.es>
2018-06-29 12:36:26 +0200

    working on 0.7 compat

--- src/Econometrics.jl
+++ src/Econometrics.jl
@@ -14,11 +14,14 @@ include("Utilities/vech.jl")
 include("Utilities/PrintDivider.jl")
 include("Utilities/PrintEstimationResults.jl")
 #include("Utilities/clc.jl") # problematic on Windows, for some reason
+#=
 # linear regression
 include("LinearRegression/lsfit.jl")
 include("LinearRegression/ols.jl")
 include("LinearRegression/tsls.jl")
 include("LinearRegression/TestStatistics.jl")
+=#
+#=
 # Bayesian
 include("Bayesian/mcmc.jl")
 # nonparametrics
@@ -29,29 +32,35 @@ include("NP/kernelweights.jl")
 include("NP/NeweyWest.jl")
 #include("NN/TrainNet.jl")
 #include("NN/AnalyzeNet.jl")
+=#
+#=
 # optimization
 include("Optimization/samin.jl")
 include("Optimization/fminunc.jl")
 include("Optimization/fmincon.jl")
+=#
+#=
 # MLE
 include("ML/mle.jl")
 include("ML/mleresults.jl")
 include("ML/Likelihoods/logit.jl")
 include("ML/Likelihoods/poisson.jl")
 include("ML/Likelihoods/normal.jl")
+=#
+#=
 # GMM
 include("GMM/gmm.jl")
 include("GMM/gmmresults.jl")
+=#
 export stnorm, trim, prettyprint, sortbyc, dstats, lag, lags, vech
 #export clc # problematic on Windows, for some reason!
 export PrintDivider, PrintEstimationResults
-export lsfit, ols, tsls, TestStatistics, NeweyWest
-export mcmc
-export npreg, kernelweights, bootstrap
-export npdensity
+#export lsfit, ols, tsls, TestStatistics, NeweyWest
+#export mcmc
+#export npreg, kernelweights, bootstrap
+#export npdensity
 #export TrainNet, AnalyzeNet
-export samin, fminunc, fmincon
-export mle, mleresults, logit, poisson, normal
-export gmm, gmmresults
-
+#export samin, fminunc, fmincon
+#export mle, mleresults, logit, poisson, normal
+#export gmm, gmmresults
 end
